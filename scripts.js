edee77ef4101ea85ccee75d08c1a97bb7fa604d0
edsonmendieta <emendietaj@gmail.com>
2016-12-27 13:41:34 -0800

    added click-listener to .markBox elements that adds new class to it.

--- scripts.js
+++ scripts.js
@@ -7,5 +7,20 @@ window.onload = function() {
 
         allBoxes[i].style.borderColor = '#DFE0B3';
         allBoxes[i].style.width = '8em';
-    }
+    } // ends i for-loop
+
+    //-----------------------------------------------------
+
+    // Add click-listener on '.markBox' elements
+
+    var markBoxes = document.getElementsByClassName('markBox');
+
+    for (var j = 0; j < markBoxes.length; j++) {
+
+        markBoxes[j].addEventListener('click', function() {
+
+            this.className += ' newClass';
+        });
+    } // ends j for-loop
+
 }
