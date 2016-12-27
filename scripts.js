6fb1a4bb2ecf486cf5a2aed4a81edfec10c0f947
edsonmendieta <emendietaj@gmail.com>
2016-12-26 19:14:34 -0800

    add window.load animation to board boxes.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,11 @@
+// When page is fully loaded...
+window.onload = function() {
 
+    var allBoxes = document.getElementsByClassName('box');
+
+    for (var i = 0; i < allBoxes.length; i++) {
+
+        allBoxes[i].style.borderColor = '#DFE0B3';
+        allBoxes[i].style.width = '8em';
+    }
+}
